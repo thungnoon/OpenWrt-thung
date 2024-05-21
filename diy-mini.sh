6e2f0c912b9f6d0880f00c783d05d0d391e45999
thungnoon <kongzheng.zhou@gmail.com>
2024-05-22 04:54:35 +0800

    Add files via upload

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
@@ -94,7 +94,7 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 
 # 修复 armv8 设备 xfsprogs 报错
-sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
+# sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 
 # 取消主题默认设置
 find package/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
@@ -103,7 +103,7 @@ find package/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/s
 sed -i '/PREROUTING/s/^#//' package/lean/default-settings/files/zzz-default-settings
 
 # 去除 iperf3-ssl
-sed -i 's/iperf3-ssl[[:space:]]*//g' target/linux/x86/Makefile
+# sed -i 's/iperf3-ssl[[:space:]]*//g' target/linux/x86/Makefile
 
 # 调整 Docker 到 服务 菜单
 sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua

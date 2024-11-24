3bc13b7c89d0f69ded4e6d78e70a9e76a9c1bf69
thungnoon <kongzheng.zhou@gmail.com>
2024-11-24 21:24:38 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -12,29 +12,21 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
-# 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-# SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+# adguardhome
+echo "src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome.git;master" >> "feeds.conf.default"
 
 # msd_lite
-git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
-git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+echo "src-git msd_lite https://github.com/ximiTech/luci-app-msd_lite.git;main" >> "feeds.conf.default"
+echo "src-git msd_lite https://github.com/ximiTech/msd_lite.git;main" >> "feeds.conf.default"
 
 # MosDNS
-git clone --depth=1 -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> "feeds.conf.default"
 
-# Alist
-git clone --depth=1 -b lua https://github.com/sbwml/luci-app-alist package/luci-app-alist
+# Poweroffdevice 
+echo "src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice.git;main" >> "feeds.conf.default"
 
-# Poweroffdevice
-git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-
-# homeproxy
-git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+# homeproxy 
+echo "src-git homeproxy https://github.com/immortalwrt/homeproxy.git;master" >> "feeds.conf.default"
 
 # mihomo
-git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"

571fcaeda20d7e94a9e88ca58af0a539aa9f4e81
thungnoon <kongzheng.zhou@gmail.com>
2024-12-12 16:09:18 +0800

    Add files via upload

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p files/usr/bin/AdGuardHome
+[ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
 AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_${1}.tar.gz"
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+wget -qO- $AGH_CORE | tar xOz > files/usr/bin/AdGuardHome/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
